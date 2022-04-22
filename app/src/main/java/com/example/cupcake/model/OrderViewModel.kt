081ab772991acc14f3325d4b9944a028b261fe74
Muntaka Helali <helali.muntaka@gmail.com>
2022-04-22 05:03:10 -0400

    Created a pickupOptionList by implementing a method using SimpleDateFormat and locale. Then used Calendar to get the current date, and the next 3 days and returned it as a list stored in a variable for easy access. Then updated the layout to display the options using binding expressions once more for the checked and onClick properties or the radio button. Implemented a resetOrder function to define the default values of the 4 main values that are defined when the shared viewModel is initialized. Finally, updated the summary fragment by adjusting the TextViews  using the viewModel variable that contain the information the user has previously selected in their order.

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -3,25 +3,30 @@ package com.example.cupcake.model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 //Shared ViewModel for Order
 class OrderViewModel: ViewModel() {
 
 //    Property for quantities of cupcakes
-    private val _quantity = MutableLiveData<Int>(0)
+    private val _quantity = MutableLiveData<Int>()
     val quantity: LiveData<Int> = _quantity
 
 //    Property for flavor of cupcakes
-    private val _flavor = MutableLiveData<String>("")
+    private val _flavor = MutableLiveData<String>()
     val flavor: LiveData<String> = _flavor
 
 //    Property for pick-up date for cupcakes
-    private val _date = MutableLiveData<String>("")
+    private val _date = MutableLiveData<String>()
     val date: LiveData<String> = _date
 
 //    Property for order price of the cupcakes
-    private val _price = MutableLiveData<Double>(0.0)
+    private val _price = MutableLiveData<Double>()
     val price: LiveData<Double> = _price
+
+//    Class property for date pickup options
+    val dateOptions = getPickupOptions()
 
 //    Setter Method to set quantity of cupcakes selected
     fun setQuantity(numberCupcakes: Int){
@@ -42,4 +47,27 @@ class OrderViewModel: ViewModel() {
         return _flavor.value.isNullOrEmpty()
     }
 
+    private fun getPickupOptions(): List<String>{
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+
+        // Create a list of dates starting with the current date and the following 3 dates
+        repeat(4){
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
+    }
+
+    fun resetOrder(){
+        _quantity.value = 0
+        _flavor.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
+    }
+
+    init{
+        resetOrder()
+    }
 }
