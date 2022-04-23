52033e8b2c38395c8388eb900dbb175aecc75ea5
Muntaka Helali <helali.muntaka@gmail.com>
2022-04-23 05:35:17 -0400

    Fixed the issue with one cupcake being selected where the email would say "1 cupcakes". In summaryFragment, store the numberOfCupcakes the user selected, and if null set that value to 0. Then used that value with quantity string by declaring a plural resource.

--- app/src/main/java/com/example/cupcake/SummaryFragment.kt
+++ app/src/main/java/com/example/cupcake/SummaryFragment.kt
@@ -65,9 +65,10 @@ class SummaryFragment : Fragment() {
      * Submit the order by sharing out the order details to another app via an implicit intent.
      */
     fun sendOrder() {
+        val numberOfCupcakes = sharedViewModel.quantity.value ?: 0
         val orderSummary = getString(
             R.string.order_details,
-            sharedViewModel.quantity.value.toString(),
+            resources.getQuantityString(R.plurals.cupcakes, numberOfCupcakes, numberOfCupcakes),
             sharedViewModel.flavor.value.toString(),
             sharedViewModel.date.value.toString(),
             sharedViewModel.price.value.toString()
@@ -77,6 +78,7 @@ class SummaryFragment : Fragment() {
             .setType("text/plain")
             .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.new_cupcake_order))
             .putExtra(Intent.EXTRA_TEXT, orderSummary)
+
         if(activity?.packageManager?.resolveActivity(intent, 0) !=null){
             startActivity(intent)
         }
