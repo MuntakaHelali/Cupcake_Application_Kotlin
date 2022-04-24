da4f1e7b24526834694755ffdd886b5e97ff38a1
Muntaka Helali <helali.muntaka@gmail.com>
2022-04-24 13:22:11 -0400

    First Created a rule to specify LiveData Objects should not call the main thread. Then created the first test function to check the value of quantity when 12 cupcakes are selected. Finally, created another test to check the price when 12 cupcakes are selected.

--- app/src/test/java/com/example/cupcake/ViewModelTests.kt
+++ app/src/test/java/com/example/cupcake/ViewModelTests.kt
@@ -1,4 +1,34 @@
 package com.example.cupcake
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.cupcake.model.OrderViewModel
+import org.junit.Rule
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+
 class ViewModelTests {
+
+//    Specifying LiveData objects to not call on the main thread(UI runs on this thread)
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+
+    @Test
+    fun quantity_twelve_cupcakes(){
+        val viewModel = OrderViewModel()
+//        LiveData Objects need to be observed to see the changes
+        viewModel.quantity.observeForever{}
+        viewModel.setQuantity(12)
+        assertEquals(12, viewModel.quantity.value)
+    }
+
+    @Test
+    fun price_twelve_cupcakes(){
+        val viewModel = OrderViewModel()
+        viewModel.price.observeForever{}
+        viewModel.setQuantity(12)
+        assertEquals("$27.00", viewModel.price.value)
+    }
+
 }
